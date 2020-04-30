f6c72eeeef6d356afb0af7c63e15e2c0d654a931
Sanjay <iamsanjaymalakar@gmail.com>
2020-04-30 21:02:11 +0600

    scaffold

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -4,8 +4,18 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Center(
-      child: Text("Some txt"),
+    home: Scaffold(
+      backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        title: Text('I am rich'),
+        backgroundColor: Colors.blueGrey[900],
+      ),
+      body: Center(
+        child: Image(
+          image: NetworkImage(
+              'https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg'),
+        ),
+      ),
     ),
   ));
 }
